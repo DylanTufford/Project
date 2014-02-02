221373472af4b7b537922285d1b5070d48d78487
Katie MacDonald <katie.mac257@gmail.com>
2014-02-02 14:02:58 -0400

    Switched to jQuery mobile for CSS

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	$.getJSON("bars.json", function(json){
 		for (var i = 0; i < json.bars.length; i++) {
 			var barName = json.bars[i].barName;
-			$('ul.barList').append('<li>' + barName + '</li>');   
+			$('ul.barList').append('<li><a href="barInfo.html">' + barName + '</a></li>').listview('refresh');   
 		}
 	});
 });

2eab2ddaafaaefc16e778f621bee5ce38269066b
Katie MacDonald <katie.mac257@gmail.com>
2014-02-08 12:19:35 -0400

    Added comments, a new bar to the jSON file, and added the oz to each special

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-	$.getJSON("bars.json", function(json){
+	$.getJSON("bars.json", function(json){//Get jSON document
+		//Go through all of the bars in the document, get the name of the bar, 
+		//& append the name to the list of bars
 		for (var i = 0; i < json.bars.length; i++) {
 			var barName = json.bars[i].barName;
 			$('ul.barList').append(
@@ -11,25 +13,34 @@ $(document).ready(function () {
 				)
 			); 
 		} 
-		$('ul.barList').listview('refresh');  
+		$('ul.barList').listview('refresh');//Update bar list
 	});
 });
 
 function getBarInfo(name){
+	//Clear lists
 	$('ul.specialsList').empty();
+
+	//Add bar name to the top of the bar specials & bar info pages
 	$('#barSpecials h1').text(name);
 	$('#barInfo h1').text(name);
-	$.getJSON("bars.json", function(json){
+
+	$.getJSON("bars.json", function(json){//Get jSON document
 		var index;
+		//Go through jSON document until it finds the bar that matches
+		//the bar name that was passed into the function
 		for (var i = 0; i < json.bars.length; i++) {
+			//If it finds a match, record the index & break out of the for loop
 			if(name === json.bars[i].barName){
 				index = i;
 				break;
 			}
 		}
+
+		//Store the indexed jSON object as a variable for easier access
 		var bar = json.bars[index];
 
-		//Get Info (address, hours, etc.)
+		//Get Info (address, hours, etc.) & add it to the bar information page
 		$('#address').text(bar.address);
 		$('#phone').text(bar.phone);
 		$('#mon').text("Monday: " + bar.hours.m);
@@ -40,22 +51,29 @@ function getBarInfo(name){
 		$('#sat').text("Saturday: " + bar.hours.sa);
 		$('#sun').text("Sunday: " + bar.hours.su);
 
-		//Get Specials
+		//Store the indexed jSON object's specials as a variable for easier access
 		var specials = bar.specials;
+		//Keep track of the day each special falls on
 		var day = null;
+		//Go through the list of specials
 		for(var i = 0; i < json.bars[index].specials.length; i++){
+			//If the special is on a new day that the previous special, add
+			//a new list divider for the new day
 			if(day !== specials[i].weekday){
 				day = json.bars[index].specials[i].weekday;
 				$('ul.specialsList').append(
 				    $('<li data-role="list-divider">').append(day)
 				);
 			}
+			//Get the type of drink, price, and volume of the special
 			var beverage = specials[i].beverage;
 			var price = specials[i].price;
+			var volume = specials[i].volume
+			//Append th special's information to the special's list
 			$('ul.specialsList').append(
-			    $('<li>').append("$" + price + " - " + beverage)
+			    $('<li>').append("$" + price + " - " + beverage + " (" + volume + "oz)")
 			);
 		}
-		$('ul.specialsList').listview('refresh');
+		$('ul.specialsList').listview('refresh');//Update specials list
 	});
 }

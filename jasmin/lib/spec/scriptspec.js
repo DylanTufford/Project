c4c249e1a1f48b54ecad09e3161866620f032d0d
Dylan <dy940109@dal.ca>
2014-02-27 18:14:50 -0400

    Semi working jasmine

--- jasmin/lib/spec/scriptspec.js
+++ jasmin/lib/spec/scriptspec.js
@@ -2,9 +2,14 @@ var script;
 
 describe("script", function() {
 
+	/*beforeEach(function() {
+    	loadFixtures("fixtures/MYFixture.html");
+    	$("#jasmine-fixtures").attr("data-role","page").trigger("create");
+  	})*/
+
 	it("should call function to display information", function(){
 
-		expect($('ul.barList')).toHaveLength(4);
+		expect($('ul.barList')).toHaveLength(4)
 
 	})
 

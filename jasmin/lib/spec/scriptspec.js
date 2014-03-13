266c2297f2a1e1711c5058f129a1e7c78be7691e
Dylan <dy940109@dal.ca>
2014-03-12 23:52:10 -0300

    Test for Split Crow button appearing

--- jasmin/lib/spec/scriptspec.js
+++ jasmin/lib/spec/scriptspec.js
@@ -1,7 +1,7 @@
 
 describe("script", function() {
 
-	it("will register a click", function() {
+	it("Finds Split Crow in list of bars", function() {
 		var $button = $("#barList li a:contains('The Split Crow')").first();
 
 		var spyEvent = spyOnEvent($button, 'click');

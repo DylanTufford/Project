02fb29daf80b26f2dff477d496bbde44b25272e0
Dylan <dy940109@dal.ca>
2014-03-13 11:00:17 -0300

    Checks that all buttons are created

--- jasmin/lib/spec/scriptspec.js
+++ jasmin/lib/spec/scriptspec.js
@@ -1,8 +1,38 @@
 
-describe("script", function() {
+describe("List of bars", function() {
 
-	it("Finds Split Crow in list of bars", function() {
+	it("Creates Split Crow button", function() {
 		var $button = $("#barList li a:contains('The Split Crow')").first();
+
+		var spyEvent = spyOnEvent($button, 'click');
+		$button.click();
+		expect('click').toHaveBeenTriggeredOn($button);
+		expect(spyEvent).toHaveBeenTriggered();
+
+  	});
+
+	it("Creates Halifax Alehouse button", function() {
+		var $button = $("#barList li a:contains('Halifax Alehouse')").first();
+
+		var spyEvent = spyOnEvent($button, 'click');
+		$button.click();
+		expect('click').toHaveBeenTriggeredOn($button);
+		expect(spyEvent).toHaveBeenTriggered();
+
+  	});
+
+	it("Creates Maxwell's Plum button", function() {
+		var $button = $("#barList li a:contains('Maxwell's Plum')").first();
+
+		var spyEvent = spyOnEvent($button, 'click');
+		$button.click();
+		expect('click').toHaveBeenTriggeredOn($button);
+		expect(spyEvent).toHaveBeenTriggered();
+
+  	});
+
+	it("Creates Toothy Moose button", function() {
+		var $button = $("#barList li a:contains('Toothy Moose')").first();
 
 		var spyEvent = spyOnEvent($button, 'click');
 		$button.click();
